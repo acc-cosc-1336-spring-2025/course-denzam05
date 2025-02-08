3f5159b17d9ca1e94f28b7cda50a38eea5ca6635
denzam05 <dennis.zambrano2@g.austincc.edu>
2025-02-08 04:29:53 +0000

    HOMEWORK2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,22 @@
 #
+
+def get_options_ratio (options, total):
+
+    ratio = options/ total
+    return ratio
+
+
+def get_faculty_rating (ratio):
+    ratio = float (ratio)
+    if (ratio <= 1.0 and ratio >= .9):
+        return 'Excellent'
+    elif (ratio <= .9 and ratio >= .8):
+        return 'Very good'
+    elif (ratio <= .8 and ratio >= .7):
+        return 'Good'
+    elif (ratio <= .7 and ratio >= .6):
+        return 'Needs Improvement'
+    elif (ratio <= .59 and ratio >= 0):
+        return 'Unacceptable'
+
+    
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,22 @@
 #
+
+from decisions import get_faculty_rating, get_options_ratio
+
+import decisions
+
+
+
+
+options= float (input('enter options value'))
+total= float (input('enter total value'))
+
+result= decisions.get_options_ratio (options, total)
+print ('get_faculty_rating:', result)
+
+faculty_rating= decisions.get_faculty_rating(result)
+print ("faculty rating:", faculty_rating)
+
+
+
+
+
