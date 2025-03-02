f34668097ce35ba941d4edc4c0c89bfb8f9f5bd5
denzam05 <dennis.zambrano2@g.austincc.edu>
2025-03-02 02:48:38 +0000

    homework 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,11 @@
 #
+
+import value_return
+
+def main():
+    print (value_return.get_hour (3800))
+    print (value_return.get_minutes (3800))
+    print (value_return.get_seconds (3800))
+
+
+main ()
