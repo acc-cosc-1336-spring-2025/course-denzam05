8ce0fe1b5b0785f7f74d148f680145f9692e20a9
denzam05 <dennis.zambrano2@g.austincc.edu>
2025-03-30 21:41:13 +0000

    correction homework6

--- src/homework/g_lists_and_tuples/main.py
+++ src/homework/g_lists_and_tuples/main.py
@@ -1,1 +1,38 @@
 #
+from lists import get_highest_list_value, get_lowest_list_value
+
+def main ():
+    while True:
+
+
+        print("\nMenu:")
+        print("1- show the list low / high values")
+        print("2- exit")
+        choice = input("choose an option:")
+
+        if choice =='1':
+            numbers=[]
+            while True:
+                try:
+                  num= int (input("enter a list value:"))
+                  numbers.append(num)
+                  if len (numbers) >=3:
+                   cont=input ("do you want to enter another value? (yes /no):").strip().lower()
+                   if cont != "yes":
+                        break
+                except ValueError:
+                   print ( "please enter a valid number.")
+        
+                 
+            if numbers:
+                    print(f"lowest value: {get_lowest_list_value (numbers)}")
+                    print (f"highest value: {get_highest_list_value (numbers)}")
+        elif choice == "2":
+            print("existing ...")
+            break
+        else:
+            print ("invalid option")
+
+main () 
+            
+
