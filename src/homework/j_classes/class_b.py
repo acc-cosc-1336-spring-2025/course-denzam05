61ccd7bea322590a2ae51782d2f1265f533e416f
denzam05 <dennis.zambrano2@g.austincc.edu>
2025-04-30 21:31:44 +0000

    hw9

--- src/homework/j_classes/class_b.py
+++ src/homework/j_classes/class_b.py
@@ -1,1 +1,19 @@
 #
+from class_a import die
+
+def main():
+    my_die=die()
+    print ("welcome to the Die roller.")
+
+    while True:
+        input("press enter to roll the die...")
+        my_die.roll()
+        print (my_die)
+
+
+        choice = input ("do you want to roll again? (yes /no):").strip().lower()
+        if choice != 'yes':
+            print ("thanks for playing")
+            break
+
+main ()
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,16 @@
 #
+import unittest
+
+
+from src.homework.j_classes.class_a import die
+
+class TestDie (unittest.TestCase):
+    def test_roll_value_in_range (self):
+        my_die = die()
+        for _ in range (3):
+            my_die.roll()
+            value = my_die.get_rolled_value()
+            self.assertIn (value, range (1, 7), f"Value {value} not in range 1 to 6")
+
+unittest.main()
+
